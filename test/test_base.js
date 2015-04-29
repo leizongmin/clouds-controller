2f5b2d98b31c327f885ae162b0b4caa8c9f58a4e
Zongmin Lei <leizongmin@gmail.com>
2015-04-29 16:17:50 +0800

    为完成

---
+++ test/test_base.js
@@ -0,0 +1,23 @@
+/**
+ * clouds-controller tests
+ *
+ * @author 老雷<leizongmin@gmail.com>
+ */
+
+var should = require('should');
+var async = require('async');
+var controller = require('../');
+
+describe('clouds-controller', function () {
+
+  it('connect', function (done) {
+
+    var s = controller.createServer();
+    s.listen();
+    s.on('listening', function () {
+      var c = controller.createConnection();
+    });
+
+  });
+
+});
