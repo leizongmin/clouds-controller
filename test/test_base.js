46816c7c640dd98fa2229b11fb892c2221695a47
Zongmin Lei <leizongmin@gmail.com>
2015-04-30 14:51:34 +0800

    测试通过

--- test/test_base.js
+++ test/test_base.js
@@ -7,17 +7,60 @@
 var should = require('should');
 var async = require('async');
 var controller = require('../');
+var EventProxy = require('../lib/utils').EventProxy;
+
 
 describe('clouds-controller', function () {
 
-  it('connect', function (done) {
+  it('message', function (done) {
 
     var s = controller.createServer();
     s.listen();
+
+    var ep = new EventProxy();
+
     s.on('listening', function () {
-      var c = controller.createConnection();
-      c.on('ready', function () {
-        c.send(c.id, 'hello, world');
+      var c1 = controller.createConnection();
+      c1.on('ready', function () {
+        ep.emit('c1', c1);
+      });
+      var c2 = controller.createConnection();
+      c1.on('ready', function () {
+        ep.emit('c2', c2);
+      });
+    });
+
+    ep.all('c1', 'c2', function (c1, c2) {
+      console.log('sending...');
+      var d1 = Math.random().toString();
+      var d2 = Math.random().toString();
+      c1.on('message', function (data) {
+        data.should.equal(d2);
+        ep.emit('d2');
+      });
+      c2.on('message', function (data) {
+        data.should.equal(d1);
+        ep.emit('d1');
+      });
+      c1.send(c2.id, d1, function (err) {
+        should.equal(err, null);
+      });
+      c2.send(c1.id, d2, function (err) {
+        should.equal(err, null);
+      });
+
+      ep.all('d1', 'd2', function () {
+        console.log('exit...');
+        c1.exit(function () {
+          ep.emit('e1', c1);
+        });
+        c2.exit(function () {
+          ep.emit('e2', c2);
+        });
+        ep.all('e1', 'e2', function () {
+          console.log('done');
+          done();
+        });
       });
     });
 
